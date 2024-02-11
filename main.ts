0ddeef3d2b8d8a537f5dc03f4935351686210fda
lu <webceoboy2011@gmail.com>
2024-02-11 22:37:54 +0800

    Add plugin:Mxmind Mindmap

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf, Platform, moment } from 'obsidian';
-import { platform } from 'os';
 let iframe: HTMLIFrameElement | null = null;
 let ready: boolean = false;
 // Remember to rename these classes and interfaces!
@@ -19,7 +18,7 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 		this.registerView(
 			VIEW_TYPE_EXAMPLE,
-			(leaf) => new ExampleView(leaf)
+			(leaf) => new MxmindIframeView(leaf)
 		);
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('network', 'Mxmind', (evt: MouseEvent) => {
@@ -28,7 +27,7 @@ export default class MyPlugin extends Plugin {
 			this.toggleView();
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('mxmind');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		// const statusBarItemEl = this.addStatusBarItem();
@@ -72,7 +71,7 @@ export default class MyPlugin extends Plugin {
 		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		//this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -85,6 +84,7 @@ export default class MyPlugin extends Plugin {
 
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
+				//@ts-ignore
 				const extension = file.extension as string
 				if (!extension || extension != 'md') return;
 				const { vault } = this.app;
@@ -173,48 +173,9 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
-export class ExampleView extends ItemView {
+export class MxmindIframeView extends ItemView {
 	navigation: boolean = false;
 	constructor(leaf: WorkspaceLeaf) {
 		super(leaf);
